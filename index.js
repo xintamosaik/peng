fe7b0cbca937fbbaf0c49d200cf1d6e02874e9c9
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-02-09 14:52:34 +0100

    add movement

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-console.log(main);
 const ctx = main.getContext("2d", { alpha: false });
+
 ctx.lineWidth = 10;
 ctx.strokeStyle = "lightgrey";
 ctx.fillStyle = "lightgrey";
+
+ctx.font = "50px monospace";
+/**
+ * Objects
+ */
 const paddle = {
     width: 40,
     height: 200,
 };
+
 const left = {
     x: 80,
     y: 360,
@@ -16,35 +22,61 @@ const right = {
     x: main.width - 120,
     y: 360,
 };
+
 const test = {x:0,y:0}
-function update() {
+/**
+ * Input
+ */
+keys = new Set()
+function onKeyDown (event) { keys.add(event.key);}
+document.addEventListener("keydown", onKeyDown) 
 
-    test.x = Math.floor(Math.random() * 10);
+function onKeyUp (event) { keys.delete(event.key)}
+document.addEventListener("keyup", onKeyUp) 
 
-    test.y = Math.floor(Math.random() * 10);
+/**
+ * Update
+ */
+function update(){ 
+console.log(keys)
+    if (keys.has("ArrowUp")) {
+        left.y -= 5
+    }
+
+    if (keys.has("ArrowDown") ) {
+        left.y += 5
+    }
+
 }
+
+/**
+ * Draw
+ */ 
 function draw() {
     ctx.clearRect(0, 0, main.width, main.height);
-    ctx.fillRect(left.x, left.y, paddle.width, paddle.height);
 
+    ctx.fillRect(left.x, left.y, paddle.width, paddle.height);
     ctx.fillRect(right.x, right.y, paddle.width, paddle.height);
 
-    ctx.fillRect(test.x,test.y,16,16); // fill in the pixel at (10,10)
 }
 
 
+/**
+ * Loop
+ */
 let start = 0;
-const stop = 1000;
+let reset = 10;
 function step (timestamp) {
     const delta = timestamp - start;
-    console.log(delta, timestamp, start);
-    update()
-    draw()
-    if (delta < stop) {
-        requestAnimationFrame(step);
+    if (delta > reset) {
+        start += reset;
+        update()
+        draw()
+
+        ctx.fillText( `start ${start} timestamp: ${Math.floor(timestamp)} delta: ${Math.floor(delta)}`, 50, 50)        
     }
+
+    requestAnimationFrame(step)
 };
-
-
 
 window.requestAnimationFrame(step)
